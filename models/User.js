8b1e9f799069c47c8400dec6bb998f0a5a5e17db
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-05 18:08:26 -0400

    user model created with virtual

--- models/User.js
+++ models/User.js
@@ -1,47 +1,49 @@
-const { Schema, model } = require('mongoose');
-const userSchema = require('./User');
+const { Schema, model } = require("mongoose");
+const userSchema = require("./User");
 
 //schema to create User model
 const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
 
-{
-username: {
-    type:String,
-    unique:true,
-    required:true,
-    trim:true
-},
-email: {
-    type:String,
-    unique:true,
-    required:true,
-    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-},
-thoughts: [
-{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Thought'
-}
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
 
-],
-friends: [
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-    }
-    
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thought",
+      },
     ],
 
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
+//creates a virtual property 'friendCount'
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-
-
-
-
-
-
-})
-
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
