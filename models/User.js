d1ea00f36ecdd09f6364facc00888e31a6110717
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-05 17:50:10 -0400

    Created User model/schema

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./User');
+
+//schema to create User model
+const userSchema = new Schema(
+
+{
+username: {
+    type:String,
+    unique:true,
+    required:true,
+    trim:true
+},
+email: {
+    type:String,
+    unique:true,
+    required:true,
+    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+},
+thoughts: [
+{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Thought'
+}
+
+],
+friends: [
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+    
+    ],
+
+
+
+
+
+
+
+
+
+})
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
